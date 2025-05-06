88d43574b9d6207870bbdea5a71faac215f2ab6c
Gabriel Boksemboim <74774177+itsUniversal@users.noreply.github.com>
2025-05-06 12:54:23 +0300

    Create test_sample.py

---
+++ test_sample.py
@@ -0,0 +1,12 @@
+# test_sample.py
+import unittest
+
+class TestMath(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(2 + 2, 4)
+
+    def test_fail(self):
+        self.assertEqual(5 - 3, 1)  # This will fail on purpose
+
+if __name__ == '__main__':
+    unittest.main()
